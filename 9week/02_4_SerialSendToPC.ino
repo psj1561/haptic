6fccbd7dfca02791050f93725f94cb1fb1e6d8cd
ZeroToOne <31840373+psj1561@users.noreply.github.com>
2019-10-23 14:28:33 +0900

    Update 02_4_SerialSendToPC.ino

--- 9week/02_4_SerialSendToPC.ino
+++ 9week/02_4_SerialSendToPC.ino
@@ -7,7 +7,7 @@ void setup() {
 void loop() {
   char buf[255];
   sprintf(buf, "%d", cnt1);
-  // "%d" 를 "%c" 로 바꾸면 어떻게 될까요?
+  // "%d" 를 "%c" 로 바꾸면 어떻게 될까요?     변수의 기본형이 char로 바뀌므로 기호를 보내기시작한다        
   
   Serial.println(buf);
   cnt1++;
